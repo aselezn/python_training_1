ef0af56da11e6435ba146aabda4862971518679a
as_selezneva <as.selezneva@severmed.com>
2022-08-16 10:36:15 +0300

    Создан вспомогательный класс Contact в отельном файле для тестовых данных

---
+++ contact.py
@@ -0,0 +1,37 @@
+
+class Contact:
+
+    def __init__(self, name, middlename, lastname, nickname, title, company, address_1, phone_home, mobile_home, phone_work, email_1, email_2,
+                 email_3, homepage, bday, bmonth, byear, aday, amonth, ayear, address_2, phone_home_2, notes, fax):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address_1 = address_1
+        self.phone_home = phone_home
+        self.mobile_home = mobile_home
+        self.phone_work = phone_work
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address_2 = address_2
+        self.phone_home_2 = phone_home_2
+        self.notes = notes
+
+
+
+
+
+
+
+
