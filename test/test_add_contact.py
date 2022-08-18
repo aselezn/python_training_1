760171e0c919e8196fdcb629dafb90c23b527c62
as_selezneva <as.selezneva@severmed.com>
2022-08-18 11:09:09 +0300

    Перенесла методы из теста во вспомогательные методы, чтобы сделать сам тест короче

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,39 +2,40 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 from model.contact import Contact
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(60)
     
     def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_contact_add_form(wd)
-        self.fill_contact_form(wd, Contact (name="name", middlename="middlename", lastname="lastname", nickname="nickname", title="title", company="company", address_1="address, 1",
+        self.login(username="admin", password="secret")
+        self.fill_contact_form(Contact (name="name", middlename="middlename", lastname="lastname", nickname="nickname", title="title", company="company", address_1="address, 1",
                                             phone_home="55555", mobile_home="22222", phone_work="33333", fax="44444", email_1="email1@mail.ru", email_2="email2@mail.ru", email_3="email3@mail.ru",
                                             homepage="homepage", bday= "1", bmonth="January", byear="2000", aday="2", amonth="February", ayear="2000", address_2="address, 2", phone_home_2="9999",
                                             notes="notes"))
-        self.enter_contact_creation(wd)
-        self.retern_to_home_page(wd)
-        self.logout(wd)
+        self.enter_contact_creation()
+        self.logout()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def retern_to_home_page(self, wd):
+    def retern_to_home_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("home").click()
         wd.get("http://localhost/addressbook/")
 
-    def enter_contact_creation(self, wd):
+    def enter_contact_creation(self):
+        wd = self.wd
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.retern_to_home_page()
 
-    def fill_contact_form(self, wd, contact):
+    def fill_contact_form(self, contact):
+        wd = self.wd
+        self.open_contact_add_form()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.name)
@@ -104,11 +105,14 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
 
-    def open_contact_add_form(self, wd):
+    def open_contact_add_form(self):
+        wd = self.wd
         wd.find_element_by_link_text("add new").click()
         wd.get("http://localhost/addressbook/edit.php")
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -117,17 +121,13 @@ class TestAddContact(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
         return True
     
     def tearDown(self):
