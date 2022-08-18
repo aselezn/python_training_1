e66b14630fdf53960f780f87582e451743700201
as_selezneva <as.selezneva@severmed.com>
2022-08-18 13:58:42 +0300

    Реализована двухуровневую архитектура тестов. Выделеы вспомогательные классы:(SessionHelper/СontactHelper/GroupHelper)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.fill_contact_form(Contact (name="name", middlename="middlename", lastname="lastname", nickname="nickname", title="title", company="company", address_1="address, 1",
+    app.contact.fill_contact_form(Contact (name="name", middlename="middlename", lastname="lastname", nickname="nickname", title="title", company="company", address_1="address, 1",
                                             phone_home="55555", mobile_home="22222", phone_work="33333", fax="44444", email_1="email1@mail.ru", email_2="email2@mail.ru", email_3="email3@mail.ru",
                                             homepage="homepage", bday= "1", bmonth="January", byear="2000", aday="2", amonth="February", ayear="2000", address_2="address, 2", phone_home_2="9999",
                                             notes="notes"))
