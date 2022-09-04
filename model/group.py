4cf4370be2a027af51f083cbe53dedc5af65db37
as_selezneva <as.selezneva@severmed.com>
2022-09-04 23:23:35 +0300

    Тема 4. Проверка равенства объектов и списков

--- model/group.py
+++ model/group.py
@@ -6,3 +6,13 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+
+#метод для репрезинтации атрибутов
+    def __repr__(self):
+        return '%s:%s' % (self.id, self.name)
+
+
+#для сравнение атрибутов логически, а не физически (до этой функции система сравнивала объекты по местонахождению в памяти)
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
