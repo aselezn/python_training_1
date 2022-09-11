c58341589a1b5a657054b57bcf225ff130a0e355
as_selezneva <as.selezneva@severmed.com>
2022-09-11 22:42:43 +0300

    Тема 4. Тест на удаление случайной группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,12 +3,12 @@ from random import randrange
 
 
 def test_del_some_group(app):
-    if app.group.count() == 0:
+    if not app.group.count():
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
     app.group.delete_group_by_index(index) #теперь удаляем случайную группу
     new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
+    assert len(old_groups) - 1 == app.group.count()
     old_groups[index:index+1] = []
     assert old_groups == new_groups
