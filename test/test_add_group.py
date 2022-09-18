ffefedeb3963c13022ff95f22a09f68b0e2af518
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-09-18 17:31:58 +0300

    Тема 5: Параметризация тестов простая

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,18 @@ from model.group import Group
 
 
 def test_add_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="fhfhfh", header="hfhfhfh", footer="fhfhhf")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count() #используем count (выступает в роли хэша), выполянется быстрее чем основная функция
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    testdata = [
+        Group(name="fhfhfh", header="hfhfhfh", footer="fhfhhf"),
+        Group(name="", header="", footer="")
+    ]
+    for group in testdata:
+        pass
+        #old_groups = app.group.get_group_list()
+        #app.group.create(group)
+        #assert len(old_groups) + 1 == app.group.count() #используем count (выступает в роли хэша), выполянется быстрее чем основная функция
+        #new_groups = app.group.get_group_list()
+        #old_groups.append(group)
+        #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 #def test_add_empty_group(app):
     #old_groups = app.group.get_group_list()
