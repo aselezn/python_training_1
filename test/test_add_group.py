f9a1f9cbd89315021d0ce2df680959d5825d2893
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-09-18 18:02:27 +0300

    Тема 5: Теперь из тестовых данных создается ондна пустая группа и 5 с рандомными данными

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,14 +6,14 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "
+    symbols = string.ascii_letters + string.digits + " "*10 #10 раз повторяем пробел
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) #генерируется случайная длина строки не превышающая максимальную
 
 
-testdata = [
-        Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20)),
-        Group(name="", header="", footer="")
-    ]
+testdata = [Group(name="", header="", footer="")] + [
+        Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20))
+        for i in range(5) #5 раз создаем группы с рандомными данными
+]
 
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])  #название параметра/источник тестовых данных/ преобразовали в список тестовые данные
@@ -26,11 +26,3 @@ def test_add_group(app, group):
     #old_groups.append(group)
     #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-#def test_add_empty_group(app):
-    #old_groups = app.group.get_group_list()
-    #group = Group(name="", header="", footer="")
-    #app.group.create(group)
-    #new_groups = app.group.get_group_list()
-    #assert len(old_groups) + 1 == len(new_groups)
-    #old_groups.append(group)
-    #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
