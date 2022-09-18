5a031b3aeb614ab6a612c1996316d2932ac8a5b7
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-09-18 17:55:54 +0300

    Тема 5: Теперь данные для создания группы генерируются автоматически

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) #генерируется случайная длина строки не превышающая максимальную
 
 
 testdata = [
-        Group(name="fhfhfh", header="hfhfhfh", footer="fhfhhf"),
+        Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20)),
         Group(name="", header="", footer="")
     ]
 
