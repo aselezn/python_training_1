ed4b4d950aaaf0a3ef26fc80fd0c83565935179a
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-09-18 23:27:51 +0300

    Тема 5/ Задание 16:  Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -3,16 +3,20 @@ from fixture.application import Application
 
 fixture = None
 
+
 @pytest.fixture
 def app(request):
     global fixture
     if fixture is None:
-        fixture = Application()
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseUrl")
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
             fixture = Application()
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
+
 
 @pytest.fixture(scope='session', autouse=True)
 def stop(request):
@@ -21,3 +25,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+
--- fixture/application.py
+++ fixture/application.py
@@ -6,11 +6,17 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Chrome()
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser) #команда raise - выполение кода в этом месте будет аварийно прервано
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
 
     def is_valid(self):
@@ -23,7 +29,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
 
 
     def is_element_present(self, how, what):
