4a1e7d1081b5f746442af8f0b6dfb631e0e92844
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-09-25 19:08:17 +0300

    Тема 6:  Конфигурационный файл для описания тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -1,20 +1,21 @@
 import pytest
 from fixture.application import Application
+import json
 
 fixture = None
-
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None: #проверка: загрузка конфигурации, если она еще не загружена и ранее не была прочитана
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -28,5 +29,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--target", action="store", default="target.json")
 
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -21,12 +21,12 @@ testdata = [Contact(name="", middlename="", lastname="", nickname="", title="", 
                       bday= "", bmonth="", byear="", aday="", amonth="", ayear="",
                       address_2="", phone_home_2="",
                       notes="")] + [
-             Contact(name=random_string("name",10), middlename=random_string("middlename",10), lastname=random_string("lastname",10), nickname=random_string("nickname",10), title=random_string("title",10),
+             Contact(name=random_string("name",10), middlename=random_string("middlename",10), lastname=random_string("lastname",10), nickname=random_string("nickname",10), company=random_string("company",10), title=random_string("title",10),
              address_1=random_string("address_1",20),
              phone_home=random_number("+79"), mobile_home=random_number("+79"), phone_work=random_number("+79"), fax=random_number("8"),
              email_1="email1@mail.ru", email_2="email2@mail.ru", email_3="email3@mail.ru",
              homepage=random_string("homepage", 10),
-             bday= "1", bmonth="January", byear="2000", aday="2", amonth="February", ayear="2000",
+             bday="1", bmonth="January", byear="2000", aday="2", amonth="February", ayear="2000",
              address_2=random_string("address_2", 20), phone_home_2=random_number("+79"),
              notes=random_string("notes", 10))
              for i in range(5)] #5 раз создаем контакт с рандомными данными
