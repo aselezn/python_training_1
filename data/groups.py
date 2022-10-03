2b62c6efb8781a2392bcac153ab63c797c7ca80b
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-10-03 22:17:57 +0300

    Тема 6: Ещё один способ параметризации тестов: динамическая генерация тестов (группы)

---
+++ data/groups.py
@@ -0,0 +1,10 @@
+from model.group import Group
+
+
+testdata = [
+    Group(name="name_1", header="header_1", footer="footer_1"),
+    Group(name="name_2", header="header_2", footer="footer_2")
+]
+
+
+
