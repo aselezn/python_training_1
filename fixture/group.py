49fa4e6f6ee977ca7e065bdd0ddcc002ed9328c6
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-10-09 20:21:17 +0300

    Тема 7:  Удаление групп (проверяем данные, загруженные из базы данных)

--- fixture/group.py
+++ fixture/group.py
@@ -56,10 +56,23 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None  #сбрасываем кеш, при следующем обращении к get_group_list будет стоиться заново в этом методе
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
 
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click() #спросить про квадратные скобочки
+
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
 
     def modify_first_group (self):
