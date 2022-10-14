57156c47b5b55cd5af98bc9229e2847b10f8035f
as.selezneva <as.selezneva@MacBook-Pro-adm.local>
2022-10-14 10:38:57 +0300

    Тема 7:  Задание 20. Исправление ошибок

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -16,9 +16,7 @@ def test_modify_some_contact(app, db, check_ui):
                       notes="notes_updated")
     contact.id = old_contacts[index].id
     app.contact.modify_contact_by_id(contact, old_contact_id)
-    new_contacts = db.get_contact_list()
-    assert len(old_contacts) == app.contact.count()
-    old_contacts.remove(contact)
+    new_contacts = db.get_contact_list(
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.group.get_contact_list(), key=Contact.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -11,8 +11,6 @@ def test_modify_some_group_name(app, db, check_ui):
     group = Group(name="new_name")
     app.group.modify_group_by_id(group, old_group_id)
     new_groups = db.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    old_groups.remove(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
